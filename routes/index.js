53f8374ad5ea9ba5151749dfa1bdb252826f93e5
m4rcosD <mp.dias023@gmail.com>
2021-11-22 13:42:15 +0000

    1.8

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
+const axios = require("axios");
+const { response } = require("express");
 const router = require("express").Router();
+const typesCusine =['Random', 'African', 'American', 'British', 'Cajun', 'Caribbean', 'Chinese', 'Eastern', 'European', 'French', 'German', 'Greek', 'Indian', 'Irish', 'Italian', 'Japanese'];
+const diet =['None','Gluten Free', 'Vegan', 'Vegetarian'];
+const intolerances =['Dairy', 'Egg', 'Gluten', 'Grain', 'Peanut', 'Seafood', 'Sesame', 'Shellfish', 'Soy' ,'Sulfite' ,'Tree', 'Nut', 'Wheat'];
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("home.hbs", {typesCusine}, {diet}, {intolerances});
+  // axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.Spoonacular_Key}`)
+  // .then((response) => {
+  //   console.log()
+  // }).catch((err) => {
+    
+  // });
 });
 
 module.exports = router;
